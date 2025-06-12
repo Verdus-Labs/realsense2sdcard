275c934fc848e4523399ac4b91fea4ff59053cf7
Rishi Gurjar <rrg85@cornell.edu>
2025-06-12 17:26:27 -0400

    ignore librealsense folder

---
+++ build/CMakeFiles/rpi_streamer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpi_streamer.dir/rpi_streamer.cpp.o"
+  "CMakeFiles/rpi_streamer.dir/rpi_streamer.cpp.o.d"
+  "rpi_streamer"
+  "rpi_streamer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpi_streamer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rpi_streamer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for rpi_streamer.
+# This may be replaced when dependencies are built.
